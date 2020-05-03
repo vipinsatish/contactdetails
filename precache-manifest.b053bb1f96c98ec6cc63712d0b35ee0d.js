b8ca7ce3802409ca50d37f9c5a4a69a2c1f510eb
vipinsatish <vipinsatish@gmail.com>
2020-05-03 06:34:47 +0530

    Updates

---
+++ precache-manifest.b053bb1f96c98ec6cc63712d0b35ee0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "489a8a7bf2d8805ac00f60f70eeaadff",
+    "url": "/contactdetails/index.html"
+  },
+  {
+    "revision": "edaa418a58c7541bc3fe",
+    "url": "/contactdetails/static/css/main.88b6eaa5.chunk.css"
+  },
+  {
+    "revision": "deaf1e2812fc56b084a9",
+    "url": "/contactdetails/static/js/2.a23a3d4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactdetails/static/js/2.a23a3d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edaa418a58c7541bc3fe",
+    "url": "/contactdetails/static/js/main.7b6c88ca.chunk.js"
+  },
+  {
+    "revision": "764eb9776373802f0ae4",
+    "url": "/contactdetails/static/js/runtime-main.06049c47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contactdetails/static/media/logo.5d5d9eef.svg"
+  }
+]);
